2f46756e6ba7fc58a07bc67ebd4fc67606e5f387
wanghaoming <whimmy@126.com>
2018-04-27 11:03:16 +0800

    use spring cloud Processor

---
+++ spring-cloud-stream-introduction/src/main/java/com/ymmihw/spring/cloud/stream/introduction/MyLoggerServiceApplication.java
@@ -0,0 +1,31 @@
+package com.ymmihw.spring.cloud.stream.introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.handler.annotation.SendTo;
+import com.ymmihw.spring.cloud.stream.introduction.model.LogMessage;
+
+@SpringBootApplication
+@EnableBinding(Processor.class)
+public class MyLoggerServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MyLoggerServiceApplication.class, args);
+  }
+
+  @Bean
+  public MessageConverter providesStringMessageConverter() {
+    return new StringMessageConverter();
+  }
+
+  @StreamListener(Processor.INPUT)
+  @SendTo(Processor.OUTPUT)
+  public LogMessage enrichLogMessage(LogMessage log) {
+    return new LogMessage(String.format("[1]: %s", log.getMessage()));
+  }
+}
